bafb37019f788e70fe088f551c23d337db376fd7
Dbo-Debugs <dsbowen87@gmail.com>
2020-05-15 17:07:04 -0400

    Made api randomly select for planet. Updated faultyItems id to be visible on submit. Changed innerHTML for fuelstatus and cargostatus to reflect if shuttle was ready or not. Updated h2 to change to green or red if shuttle is ready and text to update

--- script.js
+++ script.js
@@ -2,25 +2,69 @@
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      let missionData = [];
       let pilotNameInput = document.querySelector("input[name=pilotName]");
       let copilotNameInput = document.querySelector("input[name=copilotName]");
       let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
       let cargoMassInput = document.querySelector("input[name=cargoMass]");
-      if (pilotNameInput.value === "" || copilotNameInput.value === "" || isNaN(fuelLevelInput.value) || fuelLevelInput.value === "" || isNaN(cargoMassInput.value) || cargoMassInput.value === "") {
-         alert("All fields are required!");
-         event.preventDefault();
-      }
+      missionData.push(document.getElementById("input").value);
+      console.log(missionData);
+      // if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
+      //    alert("All fields are required!");
+      // } else if (isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value){
+      //    alert("Please enter valid information for each field!");
+      // }
+
+
+      form.addEventListener("submit", function(event) {
+         document.getElementById("faultyItems").style.visibility = "visible";
+         document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotNameInput.value} is ready for launch`;
+         document.getElementById("copilotStatus").innerHTML = `Co-Pilot ${copilotNameInput.value} is ready for launch`;
+         if (fuelLevelInput.value > 10000) {
+            document.getElementById("fuelStatus").innerHTML = `Fuel level good, ready for launch`;
+         } else if (fuelLevelInput.value < 10000) {
+            document.getElementById("fuelStatus").innerHTML = `Fuel level too low for launch`;
+         }
+         if (cargoMassInput.value < 10000) {
+            document.getElementById("cargoStatus").innerHTML = `Cargo mass low enough for launch`;
+         } else if (cargoMassInput.value > 10000) {
+            document.getElementById("cargoStatus").innerHTML = `Cargo mass too heavy for launch`;
+         }
+
+         if (fuelLevelInput.value > 10000 && cargoMassInput.value < 10000) {
+            document.getElementById("launchStatus").innerHTML = `Shuttle ready for launch`;
+            document.getElementById("launchStatus").style.color = "green";
+         } else {
+            document.getElementById("launchStatus").innerHTML = `Shuttle not ready for launch`;
+            document.getElementById("launchStatus").style.color = "red";
+         }
+
+      });
+
+
+      fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+         response.json().then(function(json) {
+            const missionTarget = document.getElementById("missionTarget");
+            form.addEventListener("submit", function(event) {
+               randomJson = json[Math.floor(Math.random() * json.length)];
+               missionTarget.innerHTML = `
+
+         <h2>Mission Destination</h2>
+   <ol>
+      <li>Name: ${randomJson.name}</li>
+      <li>Diameter: ${randomJson.diameter}</li>
+      <li>Star: ${randomJson.star}</li>
+      <li>Distance from Earth: ${randomJson.distance}</li>
+      <li>Number of Moons: ${randomJson.moons}</li>
+   </ol>
+   <img src="${randomJson.image}"></img>
+
+         `;
+            });
+         });
+      });
    });
+
 });
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
-window.onload = init;
+
